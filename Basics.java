d53b6667a8e1ec8ae41fe930357b4e861abcac9e
dillonshipley <dshipley@zagmail.gonzaga.edu>
2020-09-08 14:07:25 -0700

    Fix errors before commit 9/8/20

--- Basics.java
+++ Basics.java
@@ -14,13 +14,24 @@
  * int
  * double
  * float
+ * bool
+ * short
+ * long
+ * byte
  */
+/**
+ * Strings are nonprimitive, part of a class
+ * Can use stuff like length, equals(), compareTo(), trim(), split(), toCharArray(), charAt();
+ */
+
+import java.text.DecimalFormat;
+
 /**
  * every class must have a main
  */
 //single line comments
 
-
+import java.text.DecimalFormat;
 public class Basics {
     //all public types must be in own file
     public static void main(String[] args) {
@@ -31,5 +42,13 @@ public class Basics {
         System.out.println(args);
         char myChar = 'a';
         //the Math class is in the java.lang package which is imported for free
+        //DecimalFormat is not- check imports at top.
+        double myDouble = Math.PI;
+        DecimalFormat df = new DecimalFormat("#0.00");
+        System.out.println(df.format(myDouble));
+
+        String myString = "hello";
+        System.out.println("myString : " + myString);
+        int[] myInts = {1, 2, 3, 4};
     }
 }
