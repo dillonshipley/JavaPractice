723c62b3b5d8a805564976757bd1ff8d42b62427
dillonshipley <dshipley@zagmail.gonzaga.edu>
2020-09-09 14:48:13 -0700

    Practice session 9/9/20, added CmdLine

---
+++ CmdLine.java
@@ -0,0 +1,15 @@
+/**
+ * String[] args
+ * args is a string array that stores incoming arguments from the command line
+ */
+/**
+ * running java files
+ * javac filename.extension to compile
+ * java filename to run
+ */
+
+public class CmdLine {
+    public static void main (String[] args){
+        System.out.println(args.length);
+    }
+}
