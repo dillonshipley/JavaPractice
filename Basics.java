0712b917830f675098b0229e9ca39b829e8642f2
dillonshipley <dshipley@zagmail.gonzaga.edu>
2020-09-09 14:21:15 -0700

    Practice session 9/9/20

--- Basics.java
+++ Basics.java
@@ -1,7 +1,6 @@
 /**
  * javadoc comment!
  */
-
 /**
  * access modifiers
  * public are accesible anywhere
@@ -20,8 +19,24 @@
  * byte
  */
 /**
- * Strings are nonprimitive, part of a class
+ * strings
+ * strings are nonprimitive, part of a class
  * Can use stuff like length, equals(), compareTo(), trim(), split(), toCharArray(), charAt();
+ */
+/**
+ * cases
+ * constants use MACRO_CASE
+ * classes use PascalCase
+ * identifiers use camelCase
+ */
+/** static objects
+ * static things can be executed without creating an object of class Basics
+ * static means no "this" reference, cannot refer to class. no matter how many objects, only 1 static object
+ * String[] is bracket for args, args stores command line arguments of type string array
+ */
+/**
+ * loops
+ * for, while, do-while
  */
 
 import java.text.DecimalFormat;
@@ -32,12 +47,14 @@ import java.text.DecimalFormat;
 //single line comments
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
+
 public class Basics {
+
+    static final int NUM_FRIENDS = 4;
+
     //all public types must be in own file
     public static void main(String[] args) {
-        //static things can be executed without creating an object of class Basics
-        //static means no "this" reference, cannot refer to class
-        //String[] is bracket for args, args stores command line arguments of type string array
 
         System.out.println(args);
         char myChar = 'a';
@@ -50,5 +67,43 @@ public class Basics {
         String myString = "hello";
         System.out.println("myString : " + myString);
         int[] myInts = {1, 2, 3, 4};
+        String[] myFriends = new String[NUM_FRIENDS];
+        myFriends[0] = "p";
+        myFriends[1] = "r";
+        myFriends[2] = "m";
+        myFriends[3] = "j";
+
+        System.out.println(Arrays.toString(myFriends));
+        System.out.println(myFriends.length);
+        System.out.println(myFriends[myFriends.length - 1]);
+
+        int x = 5;
+        if (x < 5) {
+            System.out.println("x < 5");
+        } else if (x > 5) {
+            System.out.println("x > 5");
+        } else {
+            System.out.println("x is 5");
+        }
+
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.print((i * 2) + ", ");
+        }
+        System.out.println();
+
+        int i = 1;
+        while(i <= 10) {
+            System.out.print((i * 2) + ", ");
+            i++;
+        }
+        System.out.println();
+
+        i = 2;
+        do {
+            System.out.print(i + ", ");
+            i = i + 2;
+        }while(i <= 20);
+        System.out.println();
     }
 }
