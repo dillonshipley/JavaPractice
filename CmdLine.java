5443d5005877a0011cf651803963cdd0146a022a
dillonshipley <dshipley@zagmail.gonzaga.edu>
2020-09-09 14:56:43 -0700

    End of Practice session 9/9/20

--- CmdLine.java
+++ CmdLine.java
@@ -8,8 +8,19 @@
  * java filename to run
  */
 
+/**
+ * command line arguments
+ * having 2 strings for command line args is helpful- one for input file, one for output file
+ */
+
 public class CmdLine {
     public static void main (String[] args){
         System.out.println(args.length);
+        if(args.length == 3) {
+            String word = args[0];
+            int dill = Integer.parseInt(args[1]);
+            double dill2 = Double.parseDouble(args[2]);
+            System.out.println(word + " " + dill + " " + dill2);
+        }
     }
 }
